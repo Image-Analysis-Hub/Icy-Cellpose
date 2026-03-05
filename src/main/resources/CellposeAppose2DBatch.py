a46e7dd8ad209c2a9ed17c6f2e37698310cd7962
Jean-Yves Tinevez <tinevez@pasteur.fr>
2026-03-05 14:47:39 +0100

    Rework parameters templating in the script.

--- src/main/resources/CellposeAppose2DBatch.py
+++ src/main/resources/CellposeAppose2DBatch.py
@@ -5,30 +5,37 @@ import appose
 def process(img, axes):
 
     io.logger_setup()
+    
+    # Store template parameters in python variables.
+    model_name =  '${--pretrained_model}'
+    use_gpu = ${--use_gpu}
+    diameter = ${--diameter}
+    chan = ${--chan}
+    chan2 = ${--chan2}
 
     # Monkey-patch the size_model_path function to handle missing size models
     original_size_model_path = models.size_model_path    
-    def patched_size_model_path(model_type):
+    def patched_size_model_path( model_type ):
         try:
-            return original_size_model_path(model_type)
+            return original_size_model_path( model_type )
         except FileNotFoundError:
             # Return None if size model not found
             return None
     models.size_model_path = patched_size_model_path
 
-    model = models.Cellpose(model_type='${--pretrained_model}', gpu=${--use_gpu})
+    model = models.Cellpose( model_type=model_name, gpu=use_gpu )
 
     task.update(f"Original image shape: {img.shape}")
     task.update(f"Axes mapping: " + str(axes))
-    # task.update(f"Reshaped image shape: {image_reshaped.shape}")
-
+    task.update(f"Using the following parameters: diameter={diameter}, chan={chan}, chan2={chan2}, pretrained_model={model_name}, use_gpu={use_gpu}")
+    
     # Run Cellpose on each timepoint
     if 'C' in axes:
         caxis = axes['C']
         task.update(f"Using channel axis: {caxis}")
-        out = model.eval(img, diameter=${--diameter}, channels=[${--chan},${--chan2}], channel_axis=caxis, progress=True)
+        out = model.eval(img, diameter=diameter, channels=[chan,chan2], channel_axis=caxis, progress=True)
     else:
-        out = model.eval(img, diameter=${--diameter}, channels=[${--chan},${--chan2}], progress=True)	
+        out = model.eval(img, diameter=diameter, channels=[chan,chan2], progress=True)	
     masks = out[0]
     task.update(f"Mask shape: {masks.shape}")
     return masks
